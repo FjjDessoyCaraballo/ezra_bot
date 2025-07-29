7a5ded038a6150767571869eab0f9a2cd520b6bf
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-07-29 11:54:19 +0300

    feature: requesting is working. Next step is to extract all hrefs

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from html.parser import HTMLParser
+
+gods_domain = "https://web.archive.org/web/20240222194932/http://brlcenter.org/"
+holy_scriptures = []
+
+def ezra_71226(bread):
+	return
+
+def sanctimonious_html_extraction():
+	response = requests.get(gods_domain)
+	bread = response.content
+	ezra_71226(bread)
+
+sanctimonious_html_extraction()
